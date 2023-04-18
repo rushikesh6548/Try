5184bd1d084ea81bc207c0b5ed5388fd004c037e
Rushikesh Wakhare <rrushi6548@gmail.com>
2023-04-18 13:59:10 +0530

    First

---
+++ app.py
@@ -0,0 +1,11 @@
+from app2 import App2 
+import streamlit as st 
+
+import numpy as np 
+st.title("Hell")
+
+obj_app2 = App2()
+
+st.write(f"This is the number : {np.log(2)}")
+
+
---
+++ app2.py
@@ -0,0 +1,5 @@
+import numpy as np 
+class App2:
+	def __init__(self):
+		self.number = 2 
+	
